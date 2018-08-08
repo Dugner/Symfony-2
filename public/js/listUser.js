5d3b20b74857472519cf614fdcb662382f867330
Dugner <gogomkd@gmail.com>
2018-08-08 11:59:24 +0200

    with js list in admin and bondle for js /admin

--- public/js/listUser.js
+++ public/js/listUser.js
@@ -1,11 +1,31 @@
-$(function(){
-    $.ajax({
-        url: 'localhost/users.json',
-        dataType: 'jsonp',
-        success: function (data) {
-            console.log(data);        
-        }
+/*$(function(){
+    $.get(
+        '/users.json'
+    ).done(function(data){
+        let table = $('<table class="table table-striped"></table>');
+        let head = $('<thead><tr><th>Id</th><th>Name</th></tr></thead>');
+        let body = $('<tbody></tbody>');
+
+        data.forEach(function(user){
+            body.append($('<tr><td>'+user.id+'</td><td>'+user.name+'</td></tr>'));
+        });
+
+        table.append(head).append(body);
+        $('#ninja').empty().append(table);
+    }).fail(function(){
+        $('#ninja').empty().html("An Error Occured");
     });
-      // 1 - List all the category as a list of links
-      // 2 - When client on a category, show a radom joke on THIS caterogy
+
+
+
+});*/
+
+$(function(){
+    $.get(
+        Routing.generate('admin_user_list' /* your params */)
+    ).done(function(data){
+        data.forEach(function(user){
+            $('#ninja').append($('<tr><td>'+user.id+'</td><td>'+user.name+'</td></tr>'));
+        });
+    });
 });
